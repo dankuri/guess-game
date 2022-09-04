6a0bdc145bed126bafa423bd36d6be98cca6633a
samaxe13 <danek.il420@gmail.com>
2022-09-04 18:20:57 +0300

    added base guessin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess my num, pls...");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("type ur guess: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("dis not line bruv :/");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("ur guess: {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("small."),
+            Ordering::Greater => println!("big."),
+            Ordering::Equal => {
+                println!("enough.");
+                break;
+            }
+        }
+    }
 }
